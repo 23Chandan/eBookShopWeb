d001067590462604fd3b55117e5e8b6e7ea941f0
cvkumar <chandan.in63@gmail.com>
2025-03-28 17:21:58 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eBookShopWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/js/LogIn.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function () {
+    toastr.options = {
+        "closeButton": true,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "timeOut": "2000", // 2 seconds
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "extendedTimeOut": "1000"
+    };
+
+    $("#loginForm").submit(function (e) {
+        e.preventDefault();
+
+        let email = $("#email").val();
+        let password = $("#password").val();
+        let role = $("#hdUserRole").val();
+        $.ajax({
+            url: "https://localhost:7232/api/User/loginUser",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({
+                email: email,
+                password: password,
+                confirmPassword: password,
+                role: role
+            }),
+            success: function (response) {
+                toastr.success("Login successful!");
+                if (response.token) {
+                    localStorage.setItem("jwtToken", response.token);
+                }
+                setTimeout(() => {
+                    if (role == "User") {
+                        window.location.href = "/Home/Index";
+                    }
+                    else {
+                        window.location.href = "/Admin/Index";
+                    }
+                }, 1000); 
+            },
+            error: function (xhr) {
+                if (xhr.responseJSON && xhr.responseJSON.message) {
+                    toastr.error(xhr.responseJSON.message);
+                } else {
+                    toastr.error("An unexpected error occurred.");
+                }
+            }
+        });
+    });
+});
